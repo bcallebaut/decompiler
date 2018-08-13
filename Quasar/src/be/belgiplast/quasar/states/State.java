1b8bcaa37aeba0f4c7de62364cbe4b05d17c2ea4
Benoit Callebaut <benoit.callebaut@gmail.com>
2018-08-13 22:20:40 +0200

    work on state

---
+++ Quasar/src/be/belgiplast/quasar/states/State.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package be.belgiplast.quasar.states;
+
+import java.util.List;
+
+/**
+ *
+ * @author benoit
+ */
+public class State {
+    List<State> previous;
+    List<State> next;
+    
+    
+    
+}
